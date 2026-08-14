06bcc69642e8fb79e4a1e240954bd53b8df8ee3b
VIKAS GULIA <153659071+VIKASGULIA17@users.noreply.github.com>
2026-08-15 00:39:44 +0530

    fixed changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -127,7 +127,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://skill-sync-ai-lovat.vercel.app/"
+        "https://skill-sync-ai-lovat.vercel.app",
+        "https://skill-sync-ai-lovat.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
